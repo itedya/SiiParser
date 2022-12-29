bd2339747cb21803c64026d8c876976279c7a4ec
itedya <bdomanski@protonmail.com>
2022-12-29 01:42:00 +0100

    Created unit token token type

---
+++ src/token-types/unit-token.ts
@@ -0,0 +1,6 @@
+export class UnitTokenTokenType implements ITokenType{
+    public readonly regex =  new RegExp("({|}|SiiNunit)");
+    public readonly identifier = TokenIdentifier.UnitToken;
+}
+
+export default UnitTokenTokenType;
