6fdd01fb10ab63776c8be5e191e382d22e8010e9
itedya <bdomanski@protonmail.com>
2022-12-29 01:42:12 +0100

    Fixed export in new line token type

--- src/token-types/new-line.ts
+++ src/token-types/new-line.ts
@@ -2,3 +2,5 @@ export class NewLineTokenType implements ITokenType {
     public readonly regex = new RegExp("\n");
     public readonly identifier = TokenIdentifier.NewLine;
 }
+
+export default NewLineTokenType;
