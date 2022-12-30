1adec47bf362326d325425ef8d603c5901a60742
itedya <bdomanski@protonmail.com>
2022-12-30 12:26:03 +0100

    Replaced assignProperty method with getByAttributeName and setByAttribute name in GameClass
    It adds compatibility layer with @SiiAttribute decorator

--- src/game-classes/game-class.ts
+++ src/game-classes/game-class.ts
@@ -1,11 +1,26 @@
 import PropertyDoesNotExist from "../exceptions/property-does-not-exist";
+import {getSiiAttribute} from "../decorators/sii-attribute";
+
+interface IIndexable {
+    [key: string]: any
+}
 
 abstract class GameClass {
     public abstract namespace: string;
 
-    public assignProperty(propertyName: string, value: string) {
-        if (!(propertyName in this)) {
-            throw new PropertyDoesNotExist(propertyName);
+    public getByAttributeName(attributeName: string): any {
+        const propertyName = Object.keys(this).find(property => getSiiAttribute(this, property) === attributeName);
+        if (propertyName === undefined) {
+            throw new PropertyDoesNotExist(attributeName);
+        } else {
+            return (this as IIndexable)[propertyName];
+        }
+    }
+
+    public setByAttributeName(attributeName: string, value: any) {
+        const propertyName = Object.keys(this).find(property => getSiiAttribute(this, property) === attributeName);
+        if (propertyName === undefined) {
+            throw new PropertyDoesNotExist(attributeName);
         } else {
             Object.assign(this, {[propertyName]: value});
         }
