6b4543886a6c956e69138ce83aa5f5ef46ec2423
itedya <bdomanski@protonmail.com>
2023-01-15 00:26:13 +0100

    Rewritten CommentsEngine to super optimized variant

--- src/engines/comments.ts
+++ src/engines/comments.ts
@@ -1,13 +1,38 @@
 class CommentsEngine {
-    private regex: RegExp = new RegExp("((/*(.|\n)+\\*/)|(#.*))", 'g');
-
     removeComments(content: string): string {
-        const matches = [...new Set([...content.matchAll(this.regex)].map(match => match[0]))];
-        if (matches.length === 0) {
-            return content;
+        let commentStartIndex = 0;
+        let inDoubleQuotes = false;
+        let inComment = false;
+        let inMultilineComment = false;
+
+        for (let i = 0; i < content.length; i++) {
+            let prevCharacter = content[i - 1];
+            let character = content[i];
+            let nextCharacter = content[i + 1];
+
+            if (character === "\"" && prevCharacter != "\\" && !inComment && !inMultilineComment) {
+                inDoubleQuotes = !inDoubleQuotes;
+            } else {
+                if (character === "/" && nextCharacter === "*" && !inMultilineComment && !inComment) {
+                    commentStartIndex = i;
+                    inMultilineComment = true;
+                } else if (character === "#" && !inComment && !inMultilineComment) {
+                    commentStartIndex = i;
+                    inComment = true;
+                } else if (character === "\n" && inComment) {
+                    content = content.replace(content.substring(commentStartIndex, i), "");
+                    inComment = false;
+                    i = commentStartIndex - 1;
+                } else if (character === "*" && nextCharacter === "/" && inMultilineComment) {
+                    content = content.replace(content.substring(commentStartIndex, i + 1), "");
+                    inMultilineComment = false;
+                    i = commentStartIndex - 1;
+                }
+            }
         }
 
-        return content.replaceAll(this.regex, "");
+        return content;
     }
 }
+
 export default CommentsEngine;
